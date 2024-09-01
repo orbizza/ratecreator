b19a264d95c111194a336cac1e5e1d7a9a2b8903
Shaswat Deep <deepshaswat@gmail.com>
2024-09-02 00:32:36 +0530

    Rat 84 - Main Nav bar with mobile view (#9)
    * Main menu
    
    * Mobile View
    
    * reverted the styles colour

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,3 +1,10 @@
 export * from "./components/ui/button";
-export * from "./components/theme-provider";
 export * from "./components/ui/tooltip";
+export * from "./components/ui/dropdown-menu";
+export * from "./components/ui/sheet";
+export * from "./components/ui/input";
+export * from "./components/ui/label";
+export * from "./components/ui/separator";
+
+export * from "./components/themes/theme-provider";
+export * from "./components/themes/toggle-theme";
