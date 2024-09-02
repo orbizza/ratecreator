64068cc0b6e31861173786b4b9f17d9cbc3584ff
Shaswat Deep <deepshaswat@gmail.com>
2024-09-02 18:30:33 +0530

    Hero component

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,9 @@
-import Image from "next/image";
+import { LandingPage } from "@ratecreator/ui/review";
 
 export default function Home() {
-  return <main className="min-h-[calc(100vh-20vh)]">Home</main>;
+  return (
+    <main className='min-h-[calc(100vh-20vh)] mt-12 mx-10'>
+      <LandingPage />
+    </main>
+  );
 }
--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -5,6 +5,13 @@ export * from "./components/ui/sheet";
 export * from "./components/ui/input";
 export * from "./components/ui/label";
 export * from "./components/ui/separator";
+import IconCloud from "./components/ui/icon-cloud";
+export { IconCloud };
+export * from "./components/ui/sphere-mask";
+export * from "./components/ui/text-generate-effect";
+export * from "./components/ui/placeholders-and-vanish-input";
+export * from "./components/ui/linear-gradient";
+export * from "./components/ui/radial-gradient";
 
 export * from "./components/themes/theme-provider";
 export * from "./components/themes/toggle-theme";
