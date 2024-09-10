1045eb00aae0ae9764644110d0574cae57c7d5c6
Shaswat Deep <deepshaswat@gmail.com>
2024-09-10 18:37:40 +0530

    Added Clerk Auth for web

--- apps/web/app/sign-up/[[...sign-up]]/page.tsx
+++ apps/web/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,6 +1,50 @@
+"use client";
+
 import { SignUp } from "@clerk/nextjs";
 import React from "react";
 
+import { useTheme } from "next-themes";
+import { dark } from "@clerk/themes";
+import Image from "next/image";
+
 export default function Page() {
-  return <SignUp />;
+  const { theme } = useTheme();
+  const clerkTheme = theme === "dark";
+  return (
+    <div className='w-full mt-14 lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px] mb-10'>
+      <div className='flex items-center justify-center'>
+        {clerkTheme && (
+          <SignUp
+            appearance={{
+              baseTheme: dark,
+            }}
+            path='/sign-up'
+          />
+        )}
+        {!clerkTheme && <SignUp />}
+      </div>
+      <div className='hidden lg:flex items-center justify-center h-full bg-[#F1EFE7] dark:bg-black'>
+        {clerkTheme && (
+          <Image
+            src='/rc-dark.svg'
+            alt='Image'
+            layout='responsive' // Ensures the image is responsive
+            width='1920'
+            height='1080'
+            className='w-full h-auto object-cover' // Makes the image responsive and cover the container area
+          />
+        )}
+        {!clerkTheme && (
+          <Image
+            src='/rc-light.svg'
+            alt='Image'
+            layout='responsive' // Ensures the image is responsive
+            width='1920'
+            height='1080'
+            className='w-full h-auto object-cover' // Makes the image responsive and cover the container area
+          />
+        )}
+      </div>
+    </div>
+  );
 }
