d04448cd3ea8a14377791a29663602141a751c16
Shaswat Deep <deepshaswat@gmail.com>
2024-12-05 08:01:51 +0530

    Rat 125: Central search results (#32)
    * Created Search Results landing page
    
    * Added category based filter for central search results

---
+++ packages/ui/src/components/review/filters/filter-root-category-select.tsx
@@ -0,0 +1,128 @@
+"use client";
+
+import React, { useEffect, useState, useRef } from "react";
+import { useRecoilState, useResetRecoilState } from "recoil";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+  Skeleton,
+} from "@ratecreator/ui";
+import { rootCategoryFiltersState } from "@ratecreator/store/review";
+import { Info, SquareStack } from "lucide-react";
+import { Category } from "@ratecreator/types/review";
+import { getCategoryData } from "@ratecreator/actions/review";
+import { truncateText } from "@ratecreator/db/utils";
+import { useSearchParams } from "next/navigation";
+
+export const RootCategorySelect: React.FC = () => {
+  const [selectedFilters, setSelectedFilters] = useRecoilState(
+    rootCategoryFiltersState,
+  );
+  const resetRootCategory = useResetRecoilState(rootCategoryFiltersState);
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const searchParams = useSearchParams();
+  const query = searchParams.get("query");
+  const prevQueryRef = useRef(query);
+  const [rootCategoryLength, setRootCategoryLength] = useState<number>(0);
+
+  // Reset root category only when search query changes
+  useEffect(() => {
+    if (prevQueryRef.current !== query && query !== null) {
+      resetRootCategory();
+    }
+    prevQueryRef.current = query;
+  }, [query, resetRootCategory]);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        // Check for cached data in localStorage
+        const cachedCategories = localStorage.getItem(
+          "searchPageRootCategories",
+        );
+        const cacheExpiry = localStorage.getItem(
+          "searchPageRootCategoriesExpiry",
+        );
+        const currentTime = new Date().getTime();
+
+        if (
+          cachedCategories &&
+          cacheExpiry &&
+          currentTime < Number(cacheExpiry)
+        ) {
+          // Use cached data if available and not expired
+          const parsedCategories = JSON.parse(cachedCategories);
+          setCategories(parsedCategories);
+          setLoading(false);
+          setRootCategoryLength(parsedCategories.length);
+          return; // Exit early since we used cached data
+        }
+        const data = await getCategoryData();
+
+        setCategories(data as Category[]);
+
+        // Cache the fetched data in localStorage and set an expiration (e.g., 24 hours)
+        localStorage.setItem("searchPageRootCategories", JSON.stringify(data));
+        const expiryTime = new Date().getTime() + 24 * 60 * 60 * 1000; // 24 hours
+        localStorage.setItem(
+          "searchPageRootCategoriesExpiry",
+          expiryTime.toString(),
+        );
+
+        setLoading(false);
+        setRootCategoryLength(data.length);
+      } catch (err) {
+        setLoading(false);
+      }
+    };
+
+    fetchCategories();
+  }, []);
+
+  return (
+    <>
+      <div className="flex flex-col mb-2 gap-y-2">
+        <div className="flex flex-row gap-x-2 items-center">
+          <SquareStack size={16} />
+          <span className="text-[16px]">Root Categories</span>
+          <Info size={14} className="text-muted-foreground" />
+        </div>
+        {loading && (
+          <Skeleton className="h-8 w-[300px] rounded-sm shadow-md bg-neutral-50  dark:bg-neutral-950" />
+        )}
+        {!loading && (
+          <Select
+            value={selectedFilters || "all"}
+            onValueChange={(value) =>
+              setSelectedFilters(value === "all" ? "" : value)
+            }
+          >
+            <SelectTrigger className="shadow-md bg-neutral-50 dark:bg-neutral-950 text-foreground dark:text-foreground">
+              <SelectValue
+                placeholder="All Categories"
+                className="text-primary"
+              />
+            </SelectTrigger>
+            <SelectContent className="bg-neutral-50 dark:bg-neutral-950">
+              <SelectGroup>
+                <SelectItem value="all">
+                  All Categories ({rootCategoryLength})
+                </SelectItem>
+                {categories.map((item) => (
+                  <SelectItem key={item.id} value={item.slug}>
+                    {truncateText(item.name, 35)}
+                  </SelectItem>
+                ))}
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        )}
+      </div>
+    </>
+  );
+};
---
+++ packages/ui/src/components/review/search/central-search-filter-sidebar.tsx
@@ -0,0 +1,225 @@
+// components/FilterSidebar.tsx
+
+"use client";
+import React, { useEffect, useState } from "react";
+import { RouteOff, SlidersHorizontal } from "lucide-react";
+import { useResetRecoilState, useRecoilState } from "recoil";
+
+import {
+  Button,
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@ratecreator/ui";
+
+import { RatingCheckbox } from "../filters/filters-rating-select";
+import { PlatformCheckbox } from "../filters/filter-platform-select";
+import { FollowersCheckbox } from "../filters/filter-follower-select";
+import { VideoCountCheckbox } from "../filters/filter-video-select";
+import { ReviewCountCheckbox } from "../filters/filter-reviews-count-select";
+import { CountrySelect } from "../filters/filter-country-select";
+import { LanguageSelect } from "../filters/filter-language-select";
+import { MadeForKidsSelect } from "../filters/filter-kids-select";
+import { ClaimedSelect } from "../filters/filter-claimed-select";
+
+import {
+  languageFiltersState,
+  countryFiltersState,
+  followersFiltersState,
+  platformFiltersState,
+  ratingFiltersState,
+  reviewCountFiltersState,
+  videoCountFiltersState,
+  madeForKidsFilterState,
+  claimedFilterState,
+  sortByFilterState,
+  isDescendingFilterState,
+  pageNumberState,
+  rootCategoryFiltersState,
+} from "@ratecreator/store/review";
+import { RootCategorySelect } from "../filters/filter-root-category-select";
+
+interface FilterSidebarProps {}
+
+export const FilterSidebar: React.FC<FilterSidebarProps> = ({}) => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  // state reset functions
+  const resetPlatformFilters = useResetRecoilState(platformFiltersState);
+  const resetFollowersFilters = useResetRecoilState(followersFiltersState);
+  const resetRatingFilters = useResetRecoilState(ratingFiltersState);
+  const resetVideoCountFilters = useResetRecoilState(videoCountFiltersState);
+  const resetReviewCountFilters = useResetRecoilState(reviewCountFiltersState);
+  const resetCountryFilters = useResetRecoilState(countryFiltersState);
+  const resetLanguageFilters = useResetRecoilState(languageFiltersState);
+  const resetClaimedFilter = useResetRecoilState(claimedFilterState);
+  const resetMadeForKidsFilter = useResetRecoilState(madeForKidsFilterState);
+  const resetSortByFilter = useResetRecoilState(sortByFilterState);
+  const resetIsDescendingFilter = useResetRecoilState(isDescendingFilterState);
+  const resetPageNumber = useResetRecoilState(pageNumberState);
+  const resetRootCategoryFilter = useResetRecoilState(rootCategoryFiltersState);
+
+  // state selectors
+  const [platformFilters] = useRecoilState(platformFiltersState);
+  const [followersFilters] = useRecoilState(followersFiltersState);
+  const [ratingFilters] = useRecoilState(ratingFiltersState);
+  const [videoCountFilters] = useRecoilState(videoCountFiltersState);
+  const [reviewCountFilters] = useRecoilState(reviewCountFiltersState);
+  const [countryFilters] = useRecoilState(countryFiltersState);
+  const [languageFilters] = useRecoilState(languageFiltersState);
+  const [claimed] = useRecoilState(claimedFilterState);
+  const [madeForKids] = useRecoilState(madeForKidsFilterState);
+  const [sortBy] = useRecoilState(sortByFilterState);
+  const [isDescending] = useRecoilState(isDescendingFilterState);
+  const [rootCategoryFilter] = useRecoilState(rootCategoryFiltersState);
+
+  const hasActiveFilters = () => {
+    return (
+      platformFilters.length > 0 ||
+      rootCategoryFilter.length > 0 ||
+      followersFilters.length > 0 ||
+      ratingFilters.length > 0 ||
+      videoCountFilters.length > 0 ||
+      reviewCountFilters.length > 0 ||
+      countryFilters.length > 0 ||
+      languageFilters.length > 0 ||
+      claimed !== null ||
+      madeForKids !== null ||
+      sortBy !== "followed" ||
+      !isDescending
+    );
+  };
+
+  const handleClearFilters = () => {
+    resetPlatformFilters();
+    resetFollowersFilters();
+    resetRatingFilters();
+    resetVideoCountFilters();
+    resetReviewCountFilters();
+    resetCountryFilters();
+    resetLanguageFilters();
+    resetClaimedFilter();
+    resetMadeForKidsFilter();
+    resetSortByFilter();
+    resetIsDescendingFilter();
+    resetPageNumber();
+    resetRootCategoryFilter();
+  };
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  useEffect(() => {
+    resetPageNumber();
+  }, [
+    platformFilters,
+    followersFilters,
+    ratingFilters,
+    videoCountFilters,
+    reviewCountFilters,
+    countryFilters,
+    languageFilters,
+    claimed,
+    madeForKids,
+    sortBy,
+    isDescending,
+    resetPageNumber,
+    resetRootCategoryFilter,
+  ]);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  const FilterContent = () => (
+    <div className="space-y-4">
+      <PlatformCheckbox />
+      <RootCategorySelect />
+      <FollowersCheckbox />
+      <RatingCheckbox />
+      <ReviewCountCheckbox />
+      <VideoCountCheckbox />
+      {/*  ToDo: Enable Claim Status after Algolia is updated  */}
+      {/* <ClaimedSelect /> */}
+
+      <MadeForKidsSelect />
+      <CountrySelect />
+      <LanguageSelect />
+
+      <Button
+        variant="default"
+        size="sm"
+        className="w-full mt-4 gap-2 "
+        onClick={handleClearFilters}
+        disabled={!hasActiveFilters()}
+      >
+        <RouteOff size={16} className="mr-2" />
+        Clear Filters
+      </Button>
+    </div>
+  );
+  return (
+    <>
+      {/* Mobile Sheet Filter */}
+      <div className="xl:hidden">
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button
+              variant="default"
+              size="sm"
+              className="flex items-center gap-2"
+            >
+              <SlidersHorizontal size={16} />
+              <span className="hidden md:inline-block">Filters</span>
+            </Button>
+          </SheetTrigger>
+          <SheetContent
+            side="left"
+            className="w-[300px] overflow-y-auto max-h-screen"
+          >
+            <SheetHeader className="flex ">
+              <SheetTitle className="flex text-primary items-center gap-2">
+                <SlidersHorizontal size={20} />
+                Filters
+              </SheetTitle>
+            </SheetHeader>
+            <div className="mt-6">
+              <FilterContent />
+            </div>
+          </SheetContent>
+        </Sheet>
+      </div>
+
+      {/* Desktop Sidebar */}
+      <div className="hidden xl:flex mt-1 rounded-lg overflow-hidden shadow-md flex-col">
+        <div className="dark:bg-neutral-900 bg-neutral-50 p-4 flex-grow">
+          <Accordion
+            type="single"
+            collapsible
+            className="w-full"
+            defaultValue="item-1"
+          >
+            <AccordionItem value="item-1" className="border-0">
+              <AccordionTrigger className="hover:no-underline">
+                <div className="flex flex-row items-center mb-2 text-primary text-lg gap-x-2">
+                  <SlidersHorizontal size={20} />
+                  <p className="text-xl">Filters</p>
+                </div>
+              </AccordionTrigger>
+              <AccordionContent>
+                <FilterContent />
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        </div>
+      </div>
+    </>
+  );
+};
