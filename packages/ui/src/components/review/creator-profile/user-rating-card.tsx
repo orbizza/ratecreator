f005e577aeb7adbb73df07418e274b70bf6e56f5
Shaswat Deep <deepshaswat@gmail.com>
2024-12-16 18:24:27 +0530

    Minor changes in user rating card

--- packages/ui/src/components/review/creator-profile/user-rating-card.tsx
+++ packages/ui/src/components/review/creator-profile/user-rating-card.tsx
@@ -22,15 +22,15 @@ const Star = ({ filled, color }: { filled: boolean; color: string }) => {
   return (
     <svg
       className={`sm:w-10 sm:h-10 w-6 h-6 ${filled ? color : "text-gray-400 dark:text-gray-600"}`}
-      viewBox='0 0 24 24'
+      viewBox="0 0 24 24"
       fill={filled ? "currentColor" : "none"}
-      stroke='currentColor'
-      strokeWidth='1'
+      stroke="currentColor"
+      strokeWidth="1"
     >
       <path
-        d='M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z'
-        strokeLinecap='round'
-        strokeLinejoin='round'
+        d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
@@ -52,45 +52,45 @@ const UserRatingCard = ({ accountId }: { accountId: string }) => {
   };
 
   return (
-    <Card className='mt-10 mx-auto lg:mx-24 p-6 max-w-screen-md bg-card rounded-lg border'>
-      <div className='flex flex-row items-center gap-4 md:gap-x-6'>
+    <Card className="mt-10 mx-auto lg:mx-24 p-6 max-w-screen-md bg-card rounded-lg border">
+      <div className="flex flex-row items-center gap-4 md:gap-x-6">
         <div>
           {!isSignedIn && (
-            <div className='sm:w-20 sm:h-20 w-16 h-16 bg-neutral-200 dark:bg-neutral-600 rounded-full flex items-center justify-center shadow-lg'>
-              <User className='w-10 h-10 text-neutral-900 dark:text-neutral-200' />
+            <div className="sm:w-20 sm:h-20 w-16 h-16 bg-neutral-200 dark:bg-neutral-600 rounded-full flex items-center justify-center shadow-lg">
+              <User className="w-10 h-10 text-neutral-900 dark:text-neutral-200" />
             </div>
           )}
           {isSignedIn && (
-            <Avatar className='sm:w-20 sm:h-20 w-16 h-16'>
+            <Avatar className="sm:w-20 sm:h-20 w-16 h-16">
               <AvatarImage src={user?.imageUrl} />
               <AvatarFallback>
                 {getInitials(
-                  user?.fullName || user?.emailAddresses[0].toString() || ""
+                  user?.fullName || user?.emailAddresses[0].toString() || "",
                 )}
               </AvatarFallback>
             </Avatar>
           )}
         </div>
-        <div className='flex flex-col gap-2'>
-          <div className='text-sm md:text-lg lg:text-xl '>
+        <div className="flex flex-col gap-2">
+          <div className="text-sm md:text-lg lg:text-xl ">
             {user?.fullName || user?.emailAddresses[0].toString() || ""}
           </div>
           <div
             onClick={handleWriteReviewClick}
-            className='text-sm sm:text-lg md:text-xl lg:text-2xl text-primary hover:underline cursor-pointer'
+            className="text-sm sm:text-lg md:text-xl lg:text-2xl text-primary hover:underline cursor-pointer"
           >
             Write a review
           </div>
         </div>
 
         <div
-          className='flex gap-1 ml-auto'
+          className="flex gap-1 ml-auto"
           onMouseLeave={() => setHoveredRating(null)}
         >
           {[...Array(5)].map((_, index) => (
             <div
               key={index}
-              className='cursor-pointer'
+              className="cursor-pointer"
               onMouseEnter={() => setHoveredRating(index + 1)}
               onClick={() => handleStarClick(index + 1)}
             >
