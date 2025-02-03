4c18c2a6e29222dd4c6529552c0ce5d921d98da2
Shaswat Deep <deepshaswat@gmail.com>
2025-02-03 06:56:22 +0530

    Query in render-results

--- packages/ui/src/components/review/commandbar/commandbar-render-results.tsx
+++ packages/ui/src/components/review/commandbar/commandbar-render-results.tsx
@@ -7,6 +7,7 @@ import { Button, Separator } from "@ratecreator/ui";
 import { CreatorCard, CategoryCard } from "@ratecreator/ui/review";
 
 import { FilteredCategory, TabType } from "@ratecreator/types/review";
+import { useKBar } from "kbar";
 
 interface RenderResultsProps {
   filteredData: FilteredCategory[];
@@ -18,24 +19,25 @@ export const RenderResults: React.FC<RenderResultsProps> = ({
   activeTab,
 }) => {
   const router = useRouter();
+  const { query } = useKBar();
 
   const handleCategoryClick = (categoryName: string) => {
     router.push(`/categories/${encodeURIComponent(categoryName)}`);
   };
   return (
-    <div className="mt-4 max-h-[50vh] overflow-y-auto">
+    <div className='mt-4 max-h-[50vh] overflow-y-auto'>
       {filteredData.map((category) => (
-        <div key={category.name} className="mb-6">
+        <div key={category.name} className='mb-6'>
           <Button
             variant={"link"}
             onClick={() => handleCategoryClick(category.name)}
-            className="w-full text-left mb-2 hover:bg-accent rounded-md transition-colors duration-200"
+            className='w-full text-left mb-2 hover:bg-accent rounded-md transition-colors duration-200'
           >
-            <h3 className="text-sm font-semibold text-muted-foreground py-1 px-2">
+            <h3 className='text-sm font-semibold text-muted-foreground py-1 px-2'>
               {category.name}
             </h3>
           </Button>
-          <div className="space-y-2">
+          <div className='space-y-2'>
             {category.items.map((item) =>
               "isCategory" in item ? (
                 <CategoryCard key={item.name} name={item.name} />
@@ -45,14 +47,14 @@ export const RenderResults: React.FC<RenderResultsProps> = ({
                   {...item}
                   setOpen={() => query.toggle()}
                 />
-              ),
+              )
             )}
           </div>
-          <Separator className="my-4" />
+          <Separator className='my-4' />
         </div>
       ))}
       {filteredData.length === 0 && (
-        <div className="text-center text-muted-foreground">
+        <div className='text-center text-muted-foreground'>
           No results found for the current tab.
         </div>
       )}
