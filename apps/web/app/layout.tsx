36db1ffbbbf48cf86292ce8974c2708c5028e1a1
Shaswat Deep <deepshaswat@gmail.com>
2025-02-12 06:34:46 +0530

    Create review and save to DB

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang='en' suppressHydrationWarning>
+      <html lang="en" suppressHydrationWarning>
         <CSPostHogProvider>
           <body className={inter.className}>
             <Providers>
