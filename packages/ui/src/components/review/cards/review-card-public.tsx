2bd5b8a7423fc8c3bb900f9db6a0075793f72de5
Shaswat Deep <deepshaswat@gmail.com>
2025-02-15 07:24:21 +0530

    Quill review custom class

--- packages/ui/src/components/review/cards/review-card-public.tsx
+++ packages/ui/src/components/review/cards/review-card-public.tsx
@@ -41,15 +41,15 @@ const Star = ({ filled, color }: { filled: boolean; color: string }) => {
   return (
     <svg
       className={`sm:w-10 sm:h-10 w-6 h-6 ${filled ? color : "text-gray-400 dark:text-gray-600"}`}
-      viewBox='0 0 24 24'
+      viewBox="0 0 24 24"
       fill={filled ? "currentColor" : "none"}
-      stroke='currentColor'
-      strokeWidth='1'
+      stroke="currentColor"
+      strokeWidth="1"
     >
       <path
-        d='M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z'
-        strokeLinecap='round'
-        strokeLinejoin='round'
+        d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
@@ -58,7 +58,7 @@ const Star = ({ filled, color }: { filled: boolean; color: string }) => {
 const StarRating: React.FC<{ rating: number }> = ({ rating }) => {
   const color = getRatingColor(rating);
   return (
-    <div className='flex gap-1'>
+    <div className="flex gap-1">
       {[1, 2, 3, 4, 5].map((star) => (
         <Star key={star} filled={star <= rating} color={color} />
       ))}
@@ -82,9 +82,9 @@ const PlatformContent: React.FC<{ platform: string; contentUrl: string }> = ({
         return (
           <iframe
             src={convertToEmbeddedUrl(contentUrl)}
-            title='YouTube video player'
-            className='w-full h-full rounded-md shadow-md'
-            allow='accelerometer; clipboard-write; encrypted-media; gyroscope;'
+            title="YouTube video player"
+            className="w-full h-full rounded-md shadow-md"
+            allow="accelerometer; clipboard-write; encrypted-media; gyroscope;"
             allowFullScreen
           />
         );
@@ -92,8 +92,8 @@ const PlatformContent: React.FC<{ platform: string; contentUrl: string }> = ({
         return (
           <iframe
             src={`https://platform.twitter.com/embed/Tweet.html?id=${extractTweetId(contentUrl)}`}
-            className='w-full h-full object-cover rounded-md shadow-md'
-            allow='accelerometer; clipboard-write; encrypted-media; gyroscope;'
+            className="w-full h-full object-cover rounded-md shadow-md"
+            allow="accelerometer; clipboard-write; encrypted-media; gyroscope;"
             allowFullScreen
           />
         );
@@ -104,9 +104,9 @@ const PlatformContent: React.FC<{ platform: string; contentUrl: string }> = ({
   };
 
   return (
-    <div className='text-lg sm:text-xl text-primary/70 gap-4 font-semibold mt-6'>
+    <div className="text-lg sm:text-xl text-primary/70 gap-4 font-semibold mt-6">
       Supporting {platform} Content
-      <div className='relative w-full aspect-video mt-4'>{renderContent()}</div>
+      <div className="relative w-full aspect-video mt-4">{renderContent()}</div>
     </div>
   );
 };
@@ -115,79 +115,79 @@ export const ReviewCardPublic: React.FC<ReviewCardPublicProps> = ({
   review,
 }) => {
   return (
-    <Card className='w-full h-full lg:w-3/4 lg:h-auto p-6 bg-background border dark:bg-card shadow-lg'>
-      <div className='flex flex-row gap-4 justify-between'>
-        <div className='flex items-center gap-3 mb-4'>
+    <Card className="w-full h-full lg:w-3/4 lg:h-auto p-6 bg-background border dark:bg-card shadow-lg">
+      <div className="flex flex-row gap-4 justify-between">
+        <div className="flex items-center gap-3 mb-4">
           {review.author?.imageUrl && (
-            <Avatar className='sm:w-16 sm:h-16 w-12 h-12'>
+            <Avatar className="sm:w-16 sm:h-16 w-12 h-12">
               <AvatarImage src={review.author?.imageUrl} />
               <AvatarFallback>
                 {getInitials(
                   review.author?.firstName + " " + review.author?.lastName ||
                     review.author?.email ||
-                    ""
+                    "",
                 )}
               </AvatarFallback>
             </Avatar>
           )}
 
           <div>
-            <div className='font-medium'>{`${truncateText(
+            <div className="font-medium">{`${truncateText(
               review.author?.firstName + " " + review.author?.lastName ||
                 review.author?.email ||
                 "",
-              20
+              20,
             )}`}</div>
-            <div className='text-sm text-muted-foreground flex items-center gap-2'>
+            <div className="text-sm text-muted-foreground flex items-center gap-2">
               <span>{review.author?.username}</span>
             </div>
           </div>
         </div>
         <div>
-          <Button variant='ghost' size='icon' className='rounded-full'>
-            <EllipsisVertical className='w-4 h-4 sm:w-6 sm:h-6 ' />
+          <Button variant="ghost" size="icon" className="rounded-full">
+            <EllipsisVertical className="w-4 h-4 sm:w-6 sm:h-6 " />
             {/* ToDo: Add show history options */}
           </Button>
         </div>
       </div>
-      <Separator className='my-4' />
-      <div className='flex flex-row gap-2 items-center justify-between'>
+      <Separator className="my-4" />
+      <div className="flex flex-row gap-2 items-center justify-between">
         <div>
           <StarRating rating={review.stars} />
         </div>
-        <div className='text-sm sm:text-md md:text-lg lg:text-xl text-muted-foreground font-normal'>
+        <div className="text-sm sm:text-md md:text-lg lg:text-xl text-muted-foreground font-normal">
           {formatDate(new Date(review.createdAt).toISOString())}
           {review.isEdited && (
-            <span className='text-sm text-muted-foreground'>
+            <span className="text-sm text-muted-foreground">
               (edited {formatDate(new Date(review.updatedAt).toISOString())})
             </span>
           )}
         </div>
       </div>
 
-      <h1 className='font-bold mb-8 text-primary text-2xl sm:text-3xl md:text-4xl mt-4'>
+      <h1 className="font-bold mb-8 text-primary text-2xl sm:text-3xl md:text-4xl mt-4">
         {review.title}
       </h1>
 
       {typeof review.content === "object" ? (
-        <div className='prose dark:prose-invert prose-2xl max-w-none'>
+        <div className="prose dark:prose-invert prose-2xl max-w-none">
           {JSON.stringify(review.content) === "{}" ? (
-            <span className='text-lg sm:text-xl text-muted-foreground'>
+            <span className="text-lg sm:text-xl text-muted-foreground">
               No review content
             </span>
           ) : (
             <>
-              <div className='flex flex-col gap-4'>
-                <div className='text-2xl sm:text-3xl text-primary/70 font-semibold'>
+              <div className="flex flex-col gap-4">
+                <div className="text-2xl sm:text-3xl text-primary/70 font-semibold">
                   Review Content
                 </div>
-                <div className='prose dark:prose-invert prose-2xl max-w-none'>
+                <div className="prose dark:prose-invert prose-2xl max-w-none">
                   <ReactQuill
                     value={review.content}
                     readOnly={true}
-                    theme='bubble'
+                    theme="bubble"
                     modules={{ toolbar: false }}
-                    className='[&_.ql-editor]:!text-lg sm:[&_.ql-editor]:!text-xl [&_.ql-editor]:!p-0 [&_.ql-editor_p]:!text-lg sm:[&_.ql-editor_p]:!text-xl [&_.ql-editor_img]:!max-w-full [&_.ql-editor_img]:!h-auto'
+                    className="[&_.ql-editor]:!text-lg sm:[&_.ql-editor]:!text-xl [&_.ql-editor]:!p-0 [&_.ql-editor_p]:!text-lg sm:[&_.ql-editor_p]:!text-xl [&_.ql-editor_img]:!max-w-full [&_.ql-editor_img]:!h-auto"
                   />
                 </div>
               </div>
@@ -196,17 +196,17 @@ export const ReviewCardPublic: React.FC<ReviewCardPublicProps> = ({
         </div>
       ) : (
         <>
-          <div className='flex flex-col gap-4'>
-            <div className='text-lg sm:text-xl text-primary/70 font-semibold'>
+          <div className="flex flex-col gap-4">
+            <div className="text-lg sm:text-xl text-primary/70 font-semibold">
               Review Content
             </div>
-            <div className='prose dark:prose-invert prose-2xl max-w-none'>
+            <div className="prose dark:prose-invert prose-2xl max-w-none">
               <ReactQuill
                 value={review.content}
                 readOnly={true}
-                theme='bubble'
+                theme="bubble"
                 modules={{ toolbar: false }}
-                className='[&_.ql-editor]:!text-lg sm:[&_.ql-editor]:!text-xl [&_.ql-editor]:!p-0 [&_.ql-editor_p]:!text-lg sm:[&_.ql-editor_p]:!text-xl [&_.ql-editor_img]:!max-w-full [&_.ql-editor_img]:!h-auto'
+                className="[&_.ql-editor]:!text-lg sm:[&_.ql-editor]:!text-xl [&_.ql-editor]:!p-0 [&_.ql-editor_p]:!text-lg sm:[&_.ql-editor_p]:!text-xl [&_.ql-editor_img]:!max-w-full [&_.ql-editor_img]:!h-auto"
               />
             </div>
           </div>
@@ -220,44 +220,44 @@ export const ReviewCardPublic: React.FC<ReviewCardPublicProps> = ({
         />
       )}
 
-      <Separator className='my-4' />
+      <Separator className="my-4" />
 
-      <div className='text-sm mt-4 flex items-center justify-between gap-4'>
-        <div className='flex items-center gap-1 sm:gap-4'>
-          <div className='flex flex-row items-center gap-1 rounded-full p-0 border border-gray-200 dark:border-gray-800'>
-            <Button variant='ghost' size='icon' className='rounded-full'>
-              <ArrowBigUp className='w-6 h-6' />
+      <div className="text-sm mt-4 flex items-center justify-between gap-4">
+        <div className="flex items-center gap-1 sm:gap-4">
+          <div className="flex flex-row items-center gap-1 rounded-full p-0 border border-gray-200 dark:border-gray-800">
+            <Button variant="ghost" size="icon" className="rounded-full">
+              <ArrowBigUp className="w-6 h-6" />
             </Button>
             10
-            <Button variant='ghost' size='icon' className='rounded-full'>
-              <ArrowBigDown className='w-6 h-6' />
+            <Button variant="ghost" size="icon" className="rounded-full">
+              <ArrowBigDown className="w-6 h-6" />
             </Button>
           </div>
-          <div className='flex items-center gap-2'>
+          <div className="flex items-center gap-2">
             <Button
-              variant='ghost'
-              size='sm'
-              className='hover:text-foreground transition-colors rounded-full p-2 gap-2 sm:ml-2'
+              variant="ghost"
+              size="sm"
+              className="hover:text-foreground transition-colors rounded-full p-2 gap-2 sm:ml-2"
             >
-              <MessageCircle className='w-5 h-5 sm:w-6 sm:h-6' />
-              <span className='text-sm'>10</span>
+              <MessageCircle className="w-5 h-5 sm:w-6 sm:h-6" />
+              <span className="text-sm">10</span>
             </Button>
             <Button
-              variant='ghost'
-              size='sm'
-              className='hover:text-foreground transition-colors rounded-full p-2 gap-1 sm:gap-2'
+              variant="ghost"
+              size="sm"
+              className="hover:text-foreground transition-colors rounded-full p-2 gap-1 sm:gap-2"
             >
-              <Share2 className='w-5 h-5 sm:w-6 sm:h-6' />
-              <span className='text-sm'>Share</span>
+              <Share2 className="w-5 h-5 sm:w-6 sm:h-6" />
+              <span className="text-sm">Share</span>
             </Button>
           </div>
         </div>
         <Button
-          variant='ghost'
-          size='sm'
-          className='hover:text-foreground transition-colors rounded-full p-2'
+          variant="ghost"
+          size="sm"
+          className="hover:text-foreground transition-colors rounded-full p-2"
         >
-          <FlagOff className='w-5 h-5 sm:w-6 sm:h-6' />
+          <FlagOff className="w-5 h-5 sm:w-6 sm:h-6" />
         </Button>
       </div>
     </Card>
