6bd81099bf1f6e903fa6f33511d2e4cc4def2366
Shaswat Deep <deepshaswat@gmail.com>
2025-02-17 12:48:27 +0530

    refactored code to have central skeleton folder

--- packages/ui/src/components/review/categories/category-list-page.tsx
+++ packages/ui/src/components/review/categories/category-list-page.tsx
@@ -109,19 +109,19 @@ export const CategoryListPage: React.FC = () => {
         }
         const data = await getCategoryData();
         const categoriesWithColors = addColorsToCategories(
-          data as CategoryWithColor[]
+          data as CategoryWithColor[],
         );
         setCategories(categoriesWithColors);
 
         // Cache the fetched data in localStorage and set an expiration (e.g., 24 hours)
         localStorage.setItem(
           "categoriesWithColors",
-          JSON.stringify(categoriesWithColors)
+          JSON.stringify(categoriesWithColors),
         );
         const expiryTime = new Date().getTime() + 24 * 60 * 60 * 1000; // 24 hours
         localStorage.setItem(
           "categoriesWithColorsExpiry",
-          expiryTime.toString()
+          expiryTime.toString(),
         );
 
         setLoading(false);
@@ -135,7 +135,7 @@ export const CategoryListPage: React.FC = () => {
   }, []);
 
   const addColorsToCategories = (
-    categories: CategoryWithColor[]
+    categories: CategoryWithColor[],
   ): CategoryWithColor[] => {
     return categories.map((category, index) => ({
       ...category,
@@ -145,26 +145,26 @@ export const CategoryListPage: React.FC = () => {
   };
 
   return (
-    <div className='container mx-auto p-4 mt-10'>
-      <div className='flex flex-col items-start md:items-center w-full gap-y-4 pt-10 pb-14'>
-        <div className='text-2xl md:text-3xl font-bold mb-4 mx-0 sm:mx-6 md:mx-auto'>
+    <div className="container mx-auto p-4 mt-10">
+      <div className="flex flex-col items-start md:items-center w-full gap-y-4 pt-10 pb-14">
+        <div className="text-2xl md:text-3xl font-bold mb-4 mx-0 sm:mx-6 md:mx-auto">
           What are you looking for?
         </div>
-        <div className='w-full'>
+        <div className="w-full">
           <SearchBar />
         </div>
       </div>
-      <Separator className='my-0 md:my-4' />
-      <div className='mt-10 lg:mt-20 my-[4rem]'>
-        <h2 className='text-lg sm:text-xl md:text-2xl font-semibold my-4 mb-10'>
+      <Separator className="my-0 md:my-4" />
+      <div className="mt-10 lg:mt-20 my-[4rem]">
+        <h2 className="text-lg sm:text-xl md:text-2xl font-semibold my-4 mb-10">
           Explore{" "}
-          <span className='text-primary'>Creators &amp; Communities</span> by
+          <span className="text-primary">Creators &amp; Communities</span> by
           category
         </h2>
         {!loading && (
-          <div className='columns-1 sm:columns-2 md:columns-3 lg:columns-4 gap-4'>
+          <div className="columns-1 sm:columns-2 md:columns-3 lg:columns-4 gap-4">
             {categories.map((category) => (
-              <div key={category.id} className='break-inside-avoid mb-4'>
+              <div key={category.id} className="break-inside-avoid mb-4">
                 <CategoryCardListPage category={category} />
               </div>
             ))}
