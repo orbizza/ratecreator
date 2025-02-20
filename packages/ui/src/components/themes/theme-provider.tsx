52531d12239c2d77b53bd0fa31fbadc5d7a871ed
Shaswat Deep <deepshaswat@gmail.com>
2025-02-20 17:05:03 +0530

    help keyboard and sentry example

--- packages/ui/src/components/themes/theme-provider.tsx
+++ packages/ui/src/components/themes/theme-provider.tsx
@@ -4,8 +4,8 @@ import type { ThemeProviderProps as NextThemeProviderProps } from "next-themes";
 export function ThemeProvider({ children, ...props }: NextThemeProviderProps) {
   return (
     <NextThemesProvider
-      attribute='class'
-      defaultTheme='system'
+      attribute="class"
+      defaultTheme="system"
       enableSystem
       disableTransitionOnChange
       {...props}
