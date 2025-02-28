2df61cb20d5067346dcf59a2ae59bf3a37d5f6e4
Shaswat Deep <deepshaswat@gmail.com>
2025-02-28 14:45:40 +0530

    Contact link

--- packages/ui/src/components/review/contact/contact-page.tsx
+++ packages/ui/src/components/review/contact/contact-page.tsx
@@ -18,9 +18,9 @@ export const ContactFormPage = () => {
     loadTheme();
   }, [theme, resolvedTheme]);
   return (
-    <div className="container max-h-[100vh-30px] mx-auto pt-20 p-6 flex flex-wrap justify-between">
-      <div className="w-full  lg:w-1/2 mb-6 lg:mb-0 ">
-        <HeroHighlight containerClassName="lg:rounded-none lg:rounded-tl-2xl lg:rounded-bl-2xl h-[44rem]">
+    <div className='container max-h-[100vh-30px] mx-auto pt-20 p-6 flex flex-wrap justify-between'>
+      <div className='w-full  lg:w-1/2 mb-6 lg:mb-0 '>
+        <HeroHighlight containerClassName='lg:rounded-none lg:rounded-tl-2xl lg:rounded-bl-2xl h-[44rem]'>
           <motion.h1
             initial={{
               opacity: 0,
@@ -34,21 +34,20 @@ export const ContactFormPage = () => {
               duration: 0.5,
               ease: [0.4, 0.0, 0.2, 1],
             }}
-            className="text-2xl px-4 md:text-4xl lg:text-5xl font-bold text-neutral-700 dark:text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto "
+            className='text-2xl px-4 md:text-4xl lg:text-5xl font-bold text-neutral-700 dark:text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto '
           >
             <span>
-              Rate Creator provides a platform to search and review{" "}
-              <Highlight className="text-black dark:text-white">
-                creators
-              </Highlight>
-              .
+              <Highlight className='text-black dark:text-white'>
+                Rate Creator
+              </Highlight>{" "}
+              provides a platform to search and review creators.
             </span>
           </motion.h1>
         </HeroHighlight>
       </div>
-      <div className="h-[44rem] rounded-2xl lg:rounded-none lg:rounded-tr-2xl lg:rounded-br-2xl  w-full flex justify-center lg:w-1/2 bg-white/[0.96] dark:bg-black/[0.96] antialiased bg-grid-black/[0.02] dark:bg-grid-white/[0.02] relative overflow-hidden m-auto p-6">
+      <div className='h-[44rem] rounded-2xl lg:rounded-none lg:rounded-tr-2xl lg:rounded-br-2xl  w-full flex justify-center lg:w-1/2 bg-white/[0.96] dark:bg-black/[0.96] antialiased bg-grid-black/[0.02] dark:bg-grid-white/[0.02] relative overflow-hidden m-auto p-6'>
         <Spotlight
-          className="-top-40 left-0 md:left-60 md:-top-20"
+          className='-top-40 left-0 md:left-60 md:-top-20'
           fill={fill}
         />
 
