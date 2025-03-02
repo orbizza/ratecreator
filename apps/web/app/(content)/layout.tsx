a7857d2c24274e469b3223b0439e759e90238d68
Shaswat Deep <deepshaswat@gmail.com>
2025-03-02 12:37:33 +0530

    Rat 269: Content Navbar (#74)
    * Routes for content pages
    
    * Navbar for content page

---
+++ apps/web/app/(content)/layout.tsx
@@ -0,0 +1,14 @@
+import { ContentNavbar } from "@ratecreator/ui/content";
+
+export default function ContentLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="min-h-[calc(100vh-20vh)] max-w-screen-xl mx-auto mt-12">
+      <ContentNavbar />
+      {children}
+    </div>
+  );
+}
