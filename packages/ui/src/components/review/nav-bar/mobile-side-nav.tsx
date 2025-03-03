0f114c8da6d2db71ae5ccffe9c99565b0890e5ea
Shaswat Deep <deepshaswat@gmail.com>
2025-03-03 17:42:50 +0530

    URL redirect for cat glossary

--- packages/ui/src/components/review/nav-bar/mobile-side-nav.tsx
+++ packages/ui/src/components/review/nav-bar/mobile-side-nav.tsx
@@ -73,26 +73,26 @@ export function MobileSideNav() {
         <Sheet>
           <SheetTrigger asChild>
             <Button
-              variant='ghost'
-              className='hover:outline-none hover:bg-transparent focus-within:outline-none focus-within:bg-transparent'
+              variant="ghost"
+              className="hover:outline-none hover:bg-transparent focus-within:outline-none focus-within:bg-transparent"
             >
-              <Avatar className='drop-shadow-md dark:drop-shadow-lg hover:drop-shadow-2xl dark:hover:drop-shadow-2xl transition-shadow duration-200'>
+              <Avatar className="drop-shadow-md dark:drop-shadow-lg hover:drop-shadow-2xl dark:hover:drop-shadow-2xl transition-shadow duration-200">
                 <AvatarImage src={user?.imageUrl} />
                 <AvatarFallback>
                   {getInitials(
-                    user?.fullName || user?.emailAddresses[0].toString() || ""
+                    user?.fullName || user?.emailAddresses[0].toString() || "",
                   )}
                 </AvatarFallback>
               </Avatar>
             </Button>
           </SheetTrigger>
 
-          <SheetContent className='w-[320px] border-l-1 overflow-y-auto max-h-screen'>
+          <SheetContent className="w-[320px] border-l-1 overflow-y-auto max-h-screen">
             <SheetTitle>
               <SheetClose asChild>
                 <Button
-                  variant='ghost'
-                  className='items-center border-transparent rounded-md'
+                  variant="ghost"
+                  className="items-center border-transparent rounded-md"
                   size={"icon"}
                   onClick={handleSerarchClick}
                 >
@@ -101,39 +101,39 @@ export function MobileSideNav() {
               </SheetClose>
             </SheetTitle>
 
-            <SheetTitle className='p-2 flex items-center justify-center'>
+            <SheetTitle className="p-2 flex items-center justify-center">
               {user?.fullName
                 ? user.fullName
                     .split(" ")
                     .map(
                       (namePart) =>
                         namePart.charAt(0).toUpperCase() +
-                        namePart.slice(1).toLowerCase()
+                        namePart.slice(1).toLowerCase(),
                     )
                     .join(" ")
                 : user?.emailAddresses[0].toString().toLowerCase() ||
                   "Anonymous"}
             </SheetTitle>
-            <Separator className='my-4 w-[270px]' />
+            <Separator className="my-4 w-[270px]" />
             <SheetClose asChild>
               <Button
                 variant={"default"}
-                className='w-[270px]'
+                className="w-[270px]"
                 onClick={() => handleNavigation("/")}
               >
                 For creators
               </Button>
             </SheetClose>
-            <Separator className='my-4 w-[270px]' />
-            <div className='grid gap-2 py-4 w-[270px]'>
+            <Separator className="my-4 w-[270px]" />
+            <div className="grid gap-2 py-4 w-[270px]">
               <SheetClose asChild>
                 <Button
                   variant={"ghost"}
                   onClick={() => handleNavigation("/user-profile")}
-                  className='flex justify-between'
+                  className="flex justify-between"
                 >
-                  <div className='flex items-center'>
-                    <User className='mr-4 size-4' />
+                  <div className="flex items-center">
+                    <User className="mr-4 size-4" />
                     <span>Profile</span>
                   </div>
                 </Button>
@@ -142,10 +142,10 @@ export function MobileSideNav() {
                 <Button
                   variant={"ghost"}
                   onClick={() => handleNavigation("/wip")}
-                  className='flex justify-between'
+                  className="flex justify-between"
                 >
-                  <div className='flex items-center'>
-                    <Star className='mr-4 size-4' />
+                  <div className="flex items-center">
+                    <Star className="mr-4 size-4" />
                     <span>My Reviews</span>
                   </div>
                 </Button>
@@ -154,24 +154,24 @@ export function MobileSideNav() {
                 <Button
                   variant={"ghost"}
                   onClick={() => handleNavigation("/wip")}
-                  className='flex justify-between'
+                  className="flex justify-between"
                 >
-                  <div className='flex items-center'>
-                    <ClipboardList className='mr-4 size-4' />
+                  <div className="flex items-center">
+                    <ClipboardList className="mr-4 size-4" />
                     <span>My Lists</span>
                   </div>
                 </Button>
               </SheetClose>
-              <Separator className='w-[270px]' />
+              <Separator className="w-[270px]" />
 
               <SheetClose asChild>
                 <Button
                   variant={"link"}
                   onClick={() => handleNavigation("/search")}
-                  className='flex justify-between'
+                  className="flex justify-between"
                 >
-                  <div className='flex items-center'>
-                    <PenLine className='mr-4 size-4' />
+                  <div className="flex items-center">
+                    <PenLine className="mr-4 size-4" />
                     <span>Write a review</span>
                   </div>
                 </Button>
@@ -180,10 +180,10 @@ export function MobileSideNav() {
                 <Button
                   variant={"ghost"}
                   onClick={() => handleNavigation("/categories")}
-                  className='flex justify-between'
+                  className="flex justify-between"
                 >
-                  <div className='flex items-center'>
-                    <Layers3 className='mr-4 size-4' />
+                  <div className="flex items-center">
+                    <Layers3 className="mr-4 size-4" />
                     <span>Categories</span>
                   </div>
                 </Button>
@@ -204,25 +204,25 @@ export function MobileSideNav() {
                 <Button
                   variant={"ghost"}
                   onClick={() => handleNavigation("/category-glossary")}
-                  className='flex justify-between'
+                  className="flex justify-between"
                 >
-                  <div className='flex items-center'>
-                    <Library className='mr-4 size-4' />
+                  <div className="flex items-center">
+                    <Library className="mr-4 size-4" />
                     <span>Glossary</span>
                   </div>
                 </Button>
               </SheetClose>
 
-              <Separator className='w-[270px]' />
+              <Separator className="w-[270px]" />
 
               <SheetClose asChild>
                 <Button
                   variant={"ghost"}
                   onClick={() => handleNavigation("/wip")}
-                  className='flex justify-between'
+                  className="flex justify-between"
                 >
-                  <div className='flex items-center'>
-                    <Settings className='mr-4 size-4' />
+                  <div className="flex items-center">
+                    <Settings className="mr-4 size-4" />
                     <span>Settings</span>
                   </div>
                 </Button>
@@ -231,10 +231,10 @@ export function MobileSideNav() {
                 <Button
                   variant={"ghost"}
                   onClick={() => handleNavigation("/wip")}
-                  className='flex justify-between'
+                  className="flex justify-between"
                 >
-                  <div className='flex items-center'>
-                    <LifeBuoy className='mr-4 size-4' />
+                  <div className="flex items-center">
+                    <LifeBuoy className="mr-4 size-4" />
                     <span>Help</span>
                   </div>
                 </Button>
@@ -244,28 +244,28 @@ export function MobileSideNav() {
                 <Button
                   variant={"ghost"}
                   onClick={() => handleNavigation("/wip")}
-                  className='flex justify-between'
+                  className="flex justify-between"
                 >
-                  <div className='flex items-center'>
-                    <Keyboard className='mr-4 size-4' />
+                  <div className="flex items-center">
+                    <Keyboard className="mr-4 size-4" />
                     <span>Keyboard shortcuts</span>
                   </div>
                 </Button>
               </SheetClose>
 
-              <Separator className='' />
+              <Separator className="" />
 
               <SheetClose asChild>
-                <div className='flex justify-between px-2 py-2 ml-2'>
-                  <div className='flex items-center'>
-                    <SunMoon className='mr-4 size-4' />
-                    <span className='text-sm'>Dark Mode</span>
+                <div className="flex justify-between px-2 py-2 ml-2">
+                  <div className="flex items-center">
+                    <SunMoon className="mr-4 size-4" />
+                    <span className="text-sm">Dark Mode</span>
                   </div>
                   <IconToggle />
                 </div>
               </SheetClose>
 
-              <Separator className='' />
+              <Separator className="" />
 
               <SheetClose asChild>
                 <Button
@@ -275,8 +275,8 @@ export function MobileSideNav() {
                     router.push("/");
                   }}
                 >
-                  <div className='flex items-center'>
-                    <LogOut className='mr-4 size-4' />
+                  <div className="flex items-center">
+                    <LogOut className="mr-4 size-4" />
                     <span>Log out</span>
                   </div>
                 </Button>
@@ -287,17 +287,17 @@ export function MobileSideNav() {
       ) : (
         <Sheet>
           <SheetTrigger asChild>
-            <Button className='p-2' size={"icon"} variant={"outline"}>
-              <Menu size='30' />
+            <Button className="p-2" size={"icon"} variant={"outline"}>
+              <Menu size="30" />
             </Button>
           </SheetTrigger>
 
-          <SheetContent className='w-[320px] border-l-1 overflow-y-auto max-h-screen'>
+          <SheetContent className="w-[320px] border-l-1 overflow-y-auto max-h-screen">
             <SheetTitle>
               <SheetClose asChild>
                 <Button
-                  variant='ghost'
-                  className='items-center border-transparent rounded-md'
+                  variant="ghost"
+                  className="items-center border-transparent rounded-md"
                   size={"icon"}
                   onClick={handleSerarchClick}
                 >
@@ -305,27 +305,27 @@ export function MobileSideNav() {
                 </Button>
               </SheetClose>
             </SheetTitle>
-            <Separator className='my-4 w-[270px]' />
-            <div className='grid gap-2 py-4 w-[270px]'>
+            <Separator className="my-4 w-[270px]" />
+            <div className="grid gap-2 py-4 w-[270px]">
               <SheetClose asChild>
                 <Button
                   variant={"default"}
-                  className='w-[270px]'
+                  className="w-[270px]"
                   onClick={() => handleNavigation("/")}
                 >
                   For creators
                 </Button>
               </SheetClose>
-              <Separator className='my-4 w-[270px]' />
+              <Separator className="my-4 w-[270px]" />
 
               <SheetClose asChild>
                 <Button
                   variant={"link"}
                   onClick={() => handleNavigation("/search")}
-                  className='flex justify-between'
+                  className="flex justify-between"
                 >
-                  <div className='flex items-center'>
-                    <PenLine className='mr-4 size-4' />
+                  <div className="flex items-center">
+                    <PenLine className="mr-4 size-4" />
                     <span>Write a review</span>
                   </div>
                 </Button>
@@ -334,10 +334,10 @@ export function MobileSideNav() {
                 <Button
                   variant={"ghost"}
                   onClick={() => handleNavigation("/categories")}
-                  className='flex justify-between'
+                  className="flex justify-between"
                 >
-                  <div className='flex items-center'>
-                    <Layers3 className='mr-4 size-4' />
+                  <div className="flex items-center">
+                    <Layers3 className="mr-4 size-4" />
                     <span>Categories</span>
                   </div>
                 </Button>
@@ -358,26 +358,26 @@ export function MobileSideNav() {
                 <Button
                   variant={"ghost"}
                   onClick={() => handleNavigation("/category-glossary")}
-                  className='flex justify-between'
+                  className="flex justify-between"
                 >
-                  <div className='flex items-center'>
-                    <Library className='mr-4 size-4' />
+                  <div className="flex items-center">
+                    <Library className="mr-4 size-4" />
                     <span>Glossary</span>
                   </div>
                 </Button>
               </SheetClose>
 
               <SheetClose asChild>
-                <div className='flex justify-between px-2 py-2 ml-2'>
-                  <div className='flex items-center'>
-                    <SunMoon className='mr-4 size-4' />
-                    <span className='text-sm'>Dark Mode</span>
+                <div className="flex justify-between px-2 py-2 ml-2">
+                  <div className="flex items-center">
+                    <SunMoon className="mr-4 size-4" />
+                    <span className="text-sm">Dark Mode</span>
                   </div>
                   <IconToggle />
                 </div>
               </SheetClose>
 
-              <Separator className='w-[270px]' />
+              <Separator className="w-[270px]" />
               <SheetClose asChild>
                 <Button
                   variant={"default"}
@@ -386,8 +386,8 @@ export function MobileSideNav() {
                     router.push(`/sign-up?redirect_url=${returnUrl}`);
                   }}
                 >
-                  <div className='flex items-center'>
-                    <UserPlus className='mr-4 size-4' />
+                  <div className="flex items-center">
+                    <UserPlus className="mr-4 size-4" />
                     <span>Get Started</span>
                   </div>
                 </Button>
@@ -400,8 +400,8 @@ export function MobileSideNav() {
                     router.push(`/sign-in?redirect_url=${returnUrl}`);
                   }}
                 >
-                  <div className='flex items-center'>
-                    <LogIn className='mr-4 size-4' />
+                  <div className="flex items-center">
+                    <LogIn className="mr-4 size-4" />
                     <span>Log in</span>
                   </div>
                 </Button>
