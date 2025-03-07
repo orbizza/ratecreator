c4b8f1a22028fd9802b3cecd47650420bcabe5db
Shaswat Deep <deepshaswat@gmail.com>
2025-03-07 19:14:24 +0530

    update the content admin repo

--- apps/content/app/layout.tsx
+++ apps/content/app/layout.tsx
@@ -16,11 +16,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body className={`${inter.className} antialiased`}>
         <ThemeProvider
-          attribute='class'
-          defaultTheme='system'
+          attribute="class"
+          defaultTheme="system"
           enableSystem
           disableTransitionOnChange
         >
