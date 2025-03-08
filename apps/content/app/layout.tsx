e53f5980c2e35e83827ed9564bd868b5ab86edac
Shaswat Deep <deepshaswat@gmail.com>
2025-03-08 14:04:58 +0530

    Clerk Setup

--- apps/content/app/layout.tsx
+++ apps/content/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@ratecreator/ui";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Nunito({ subsets: ["latin"] });
 
@@ -16,17 +17,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased`}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang='en'>
+        <body className={`${inter.className} antialiased`}>
+          <ThemeProvider
+            attribute='class'
+            defaultTheme='system'
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
