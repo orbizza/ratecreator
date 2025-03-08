a5d5a4229d1eae600430873568f09196bec6afb7
Shaswat Deep <deepshaswat@gmail.com>
2025-03-08 14:05:15 +0530

    Clerk and repo setup

--- apps/content/app/layout.tsx
+++ apps/content/app/layout.tsx
@@ -18,11 +18,11 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang='en'>
+      <html lang="en">
         <body className={`${inter.className} antialiased`}>
           <ThemeProvider
-            attribute='class'
-            defaultTheme='system'
+            attribute="class"
+            defaultTheme="system"
             enableSystem
             disableTransitionOnChange
           >
