cf8f120b5132881799e61c3a1621154cf34df326
Shaswat Deep <deepshaswat@gmail.com>
2025-04-09 00:59:40 +0530

    Resetting the code to check in preview

--- packages/ui/src/components/content/admin-posts/edit-content-post.tsx
+++ packages/ui/src/components/content/admin-posts/edit-content-post.tsx
@@ -69,7 +69,7 @@ export const EditContentPost = ({
             imageUrl: tag.tag.imageUrl ?? "",
             slug: tag.tag.slug,
             posts: tag.tag.posts,
-          }))
+          })),
         );
       } catch (error) {
         console.error("Error fetching tags:", error);
@@ -143,7 +143,7 @@ export const EditContentPost = ({
       dynamic(() => import("../../common/blocknote-editor/editor"), {
         ssr: false,
       }),
-    []
+    [],
   );
 
   const handleEditorContentChange = (content: string) => {
@@ -203,19 +203,19 @@ export const EditContentPost = ({
   };
 
   return (
-    <div className='flex relative min-h-screen'>
-      <div className='flex-1 transition-all duration-200'>
-        <div className='mx-auto rounded-md lg:max-w-screen-2xl'>
-          <div className='ml-10 max-w-screen-md lg:max-w-screen-lg'>
+    <div className="flex relative min-h-screen">
+      <div className="flex-1 transition-all duration-200">
+        <div className="mx-auto rounded-md lg:max-w-screen-2xl">
+          <div className="ml-10 max-w-screen-md lg:max-w-screen-lg">
             <UploadComponent
               imageUrl={post.featureImage}
               isSubmitting={isSubmitting}
               onChange={handleFeatureImageChange}
               isFileUploadOpen={isFeatureFileUploadOpen}
               toggleFileUpload={toggleFeatureImageUpload}
-              text='Add feature image'
-              className='text-neutral-400 font-light !no-underline hover:text-neutral-200 mt-10'
-              buttonVariant='link'
+              text="Add feature image"
+              className="text-neutral-400 font-light !no-underline hover:text-neutral-200 mt-10"
+              buttonVariant="link"
               onCancel={handleCancelUpload}
             />
           </div>
@@ -223,11 +223,11 @@ export const EditContentPost = ({
             <input
               value={post.title}
               onChange={handleMainInputChange}
-              placeholder='Post title'
-              className='w-full ml-12 mt-4 bg-transparent text-5xl font-semibold outline-none ring-0 placeholder:text-neutral-700'
+              placeholder="Post title"
+              className="w-full ml-12 mt-4 bg-transparent text-5xl font-semibold outline-none ring-0 placeholder:text-neutral-700"
             />
           </div>
-          <div className='mt-8'>
+          <div className="mt-8">
             <Editor
               onChange={handleEditorContentChange}
               initialContent={post.content}
