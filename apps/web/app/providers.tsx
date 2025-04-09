78cde295e50aa5cf90d4de6d7836d9bc3368215d
Shaswat Deep <deepshaswat@gmail.com>
2025-04-09 09:49:06 +0530

    Made some changes to handle error

--- apps/web/app/providers.tsx
+++ apps/web/app/providers.tsx
@@ -16,8 +16,8 @@ export const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider
-        attribute='class'
-        defaultTheme='system'
+        attribute="class"
+        defaultTheme="system"
         enableSystem
         disableTransitionOnChange
       >
